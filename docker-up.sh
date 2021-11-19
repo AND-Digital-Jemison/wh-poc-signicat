c99a5c9ace097b820d7d0e6aa7c17edad978b11d
Jon Earl <jon.earl@and.digital>
2021-11-19 15:23:09 +0000

    using seperate port within dockerised app

--- docker-up.sh
+++ docker-up.sh
@@ -1,4 +1,8 @@
 docker stop signicat-poc
 docker container prune -f
+
+cp .env .temp-env
+cat .temp-env
 docker build . -t wh-poc-signicat
-docker container run --name signicat-poc -d -p $1:5000 --env-file ./.env wh-poc-signicat 
+docker container run --name signicat-poc -d -p $1:5004 --env-file ./.temp-env wh-poc-signicat
+rm -rf .temp-env

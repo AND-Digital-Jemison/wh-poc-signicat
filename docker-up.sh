ff5c0b7804cf7f8ada3987977c85fd2eae1cef92
Jon Earl <jon.earl@and.digital>
2021-11-22 15:44:43 +0000

    default port to 5000 in docker version

--- docker-up.sh
+++ docker-up.sh
@@ -5,5 +5,5 @@ cp .env .temp-env
 echo "PUBLISHED_PORT=$1" >> .temp-env
 cat .temp-env
 docker build . -t wh-poc-signicat
-docker container run --name signicat-poc -d -p $1:5004 --env-file ./.temp-env wh-poc-signicat
+docker container run --name signicat-poc -d -p $1:5000 --env-file ./.temp-env wh-poc-signicat
 rm -rf .temp-env
